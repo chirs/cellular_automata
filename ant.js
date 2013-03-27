498f9638a31c3f5b658b07a6f55fb09d6ffab5cc
Chris Edgemon <chrisedgemon@gmail.com>
2013-03-27 15:17:49 -0400

    Add support for multiple colors in ant.js, clean up some of the move-making logic.

--- ant.js
+++ ant.js
@@ -1,8 +1,41 @@
+// An implementation of Langton's ant.
+// http://en.wikipedia.org/wiki/Langton's_ant
+// 
+
 
 var makeAnt = function(){
-  var coord = null;
-  var blackSquares = {};
-  var orientation = 'up';
+  var totalColors = 2;
+
+
+  // This manages cell state.
+  var cellColor = function(){
+    var cellMap = {}
+
+    var g = function(key){
+      if (key in cellMap){
+        return cellMap[key]
+      } else {
+        return 0;
+      }
+    };
+
+    var s = function(key){
+      cellMap[key] = (g(key) + 1) % totalColors;
+    };
+
+    var c = function(key){
+      return [
+        "#000",
+        "#fff",
+      ][g(key)]
+    };
+
+      return { get: g, set: s, getColor: c }
+  }();
+
+
+  var coord = [0,0]; 
+  var orientation = 'up'; // Need to improve this.
 
   var turnRight = function(o){
     orientation = {
@@ -22,12 +55,12 @@ var makeAnt = function(){
     }[orientation];
   };
 
+  // get the next coord we'll be visiting.
   var getNewCoord = function(){
 
     if (coord == null){
       return [0,0];
     }
-
     var d =  {
       'up': [1,0],
       'left': [0,-1],
@@ -38,23 +71,27 @@ var makeAnt = function(){
     return [coord[0] + d[0], coord[1] + d[1]]
   };
 
+  var actionMap = [
+    turnRight,
+    turnLeft,
+  ]
+
+
+  // Just return the function?
   return {
 
-    getCoord: function(){ return coord;  },
-    getBlackSquares: function(){ return blackSquares; },
-    getOrientation: function(){ return orientation; },
-    isBlack: function(){ return coord in blackSquares; },
-
     move: function(){
-      coord = getNewCoord();      
-      if (coord in blackSquares) {
-        turnLeft();
-        delete blackSquares[coord];
-      } else {
-        turnRight();
-        blackSquares[coord] = true;
-      }
+      var color = cellColor.get(coord);
+      //console.log(color);
+      var action = actionMap[color];
+      cellColor.set(coord);
+      action();
+      coord = getNewCoord();
 
+      return {
+        coord: coord,
+        color: cellColor.getColor(coord),
+      }
     }
   };
 
